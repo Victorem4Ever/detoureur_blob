960d8f70bae87add02cca8014a8289694a66945c
Beauruelle Romain <r3op.scopti@gmail.com>
2022-01-27 17:33:26 +0100

    fix a potential 'not enough values to unpack'

--- detoureur.py
+++ detoureur.py
@@ -51,9 +51,9 @@ class Detoureur:
     def mean(self, pixels: list):
         pixel = pixels.pop()
 
-        mean_r, mean_g, mean_b, trash = self.img.getpixel(pixel)
+        mean_r, mean_g, mean_b = self.img.getpixel(pixel)[:3]
         for pixel in pixels:
-            r, g, b, trash = self.img.getpixel(pixel)
+            r, g, b = self.img.getpixel(pixel)[:3]
             mean_r += r
             mean_r //= 2
 
@@ -76,7 +76,7 @@ class Detoureur:
         for x in range(self.x):
             for y in range(self.y):
                 pixel = (x,y)
-                r, g, b, trash = self.img.getpixel(pixel)
+                r, g, b = self.img.getpixel(pixel)[:3]
 
                 if not (abs(r-r1) <= value) or not (abs(g-g1) <= value) or not (abs(b-b1) <= value):
                     bg_pixels.append((x,y))
